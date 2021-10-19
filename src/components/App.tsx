a4a9c7aa1237c3d3216036a9b92e77f74f33ea77
Velislav Gerov <velislav.gerov@gmail.com>
2021-10-19 20:00:29 +0300

    Writeup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import formInstructions from '../data/form_instructions.json';
+import JobForm from './JobForm';
 import JobFormControlled from './JobFormControlled'
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
       <h1>👋 Hello from Team Frontier!</h1>
       <p>Good luck with the exercise. If you have any questions please email Jason: jason@frontier.jobs</p>
       <h1>👋 Hello from Velislav!</h1>
-      <p>Thank you for the wishes and for the opportunity to work on this exerciece. Below, you will find the result of my work followed by a self report.</p>
+      <p>Thank you for the wishes and for the opportunity to work on this exerciece. Below, you will find the result of my work followed by a self-report.</p>
       <h2>Result</h2>
       <JobFormControlled
         theme={job.theme}
@@ -23,7 +24,7 @@ function App() {
       />
       <h2>Self-report</h2>
       <h3>Time</h3>
-      <p>I spent about a total of 8 hours working on the exercise. I checked out a git branch called <em>solution</em> to be used as refferece for my progress through time (I took some longer breaks after finishing with the styles). The following table breaks down the timing:</p>
+      <p>I spent about a total of 16 hours working on the exercise - 8 hours on creating an uncontrolled component and 8 hours on another iteration as I was not happy with the usability of the first component. I checked out a git branch called <em>solution</em> to be used as referece for my progress. The following table breaks down the timing and tasks:</p>
       <table>
         <thead>
           <tr>
@@ -51,7 +52,7 @@ function App() {
           <tr>
             <td>2.5h</td>
             <td>Completed the styles</td>
-            <td>I am used to using styled components but desided to go "bare-bones" for the exercise. Definitely have to get faster and better with CSS.</td>
+            <td>I am used to using ui components but desided to go "bare-bones" for the exercise. Definitely have to get faster and better with CSS.</td>
           </tr>
           <tr>
             <td>.5h</td>
@@ -65,12 +66,23 @@ function App() {
           </tr>
           <tr>
             <td>3h</td>
-            <td>Added controlled JobFrom component with better composition and data types</td>
+            <td>Added JobFromControlled component with better composition and data types</td>
+            <td>N/A</td>
+          </tr>
+          <tr>
+            <td>1h</td>
+            <td>Added react-select</td>
+            <td>I was not happy with the native multiselect and decided to substitute it.</td>
+          </tr>
+          <tr>
+            <td>3h</td>
+            <td>Controlled validation</td>
             <td>N/A</td>
           </tr>
         </tbody>
       </table>
-      <h3>Solution</h3>
+      <h3>JobForm (Uncontrolled)</h3>
+      <h4>Solution</h4>
       <ul>
         <li>Tried to address all of the requirements in the exercise description without bringing in any additional packages</li>
         <li>Decided to implement an uncontrolled form and let the HTMLFormElement handle data and validation for the sake of simplicity and performance</li>
@@ -83,15 +95,28 @@ function App() {
           </ul>
         </li>
       </ul>
-      <h3>Limitations</h3>
+      <h4>Limitations</h4>
       <ul>
         <li>Styles are not too flexible (and pretty) and the form is fixed</li>
         <li>Did not go into full-depth with styling. I.e. Boolean fields render as Yes/No radio buttons</li>
         <li>Uncertanties about the type of the output data</li>
         <li>No extensive test</li>
       </ul>
+      <h3>JobFormControlled</h3>
+      <h4>Solution</h4>
+      <ul>
+        <li>Better composition</li>
+        <li>Has validation messeges</li>
+      </ul>
+      <h4>Limitations</h4>
+      <ul>
+        <li>Inline styles</li>
+        <li>Depends on react-select</li>
+        <li>Does not focus first invalid input on submit</li>
+      </ul>
       <h3>Thoughts</h3>
       <p>I liked the excerice but I was a bit rusty and should work on my performance. Despite that I didn't find the requirements too challenging as the description for the exercise was very clear.</p>
+      <p>Can't wait to hear your feedback and thoughs! Thanks!</p>
     </div >
   );
 }
